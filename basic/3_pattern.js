36e8d7daf98e473ac4eff13f7f74b116c7500f91
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-28 22:07:32 +0530

    feat(patterns): add triangle pattern

--- basic/3_pattern.js
+++ basic/3_pattern.js
@@ -2,7 +2,7 @@ let prompt = require('prompt-sync')();
 let n = Number(prompt("Enter your number:"));
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= i; j++) {
-        process.stdout.write("# ")
+        process.stdout.write("#")
     }
     console.log();
 }
