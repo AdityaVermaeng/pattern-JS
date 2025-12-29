c77973b913c294cf5431006a858107e1b99f2e80
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-29 11:50:21 +0530

    feat(patterns): add number  triangle start from 5 pattern

---
+++ number/2_pattern.js
@@ -0,0 +1,10 @@
+let prompt = require('prompt-sync')();
+let n = Number(prompt("Enter a number:"));
+for (let i = 1; i <= n; i++) {
+    let num = 5;
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(num + " ");
+        num++;
+    }
+    console.log();
+}
