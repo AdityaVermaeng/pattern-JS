a0371b29271bce18ef35d44992bda47409f4cbe5
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-29 15:39:25 +0530

    feat(patterns): add odd-only  Floyd's triangle  pattern

---
+++ number/8_pattern.js
@@ -0,0 +1,10 @@
+let prompt = require('prompt-sync')();
+let n = Number(prompt("Enter your number:"));
+let num = 1;
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        process.stdout.write(num + " ");
+        num += 2;
+    }
+    console.log();
+}
